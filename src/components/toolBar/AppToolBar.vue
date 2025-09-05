e8330bda74c9e091ddc44b473953f82e1f750e8e
stread <stread.chou@gmail.com>
2025-09-05 17:55:23 +0800

    增加文档

--- src/components/toolBar/AppToolBar.vue
+++ src/components/toolBar/AppToolBar.vue
@@ -8,12 +8,36 @@ const viewStore = useViewStore()
 <template>
   <q-toolbar>
     <!-- 左侧折叠之后才能看到关闭按钮 -->
-    <template v-if="$q.platform.is.mac && !viewStore.leftDrawerOpen">
-      <MacControl class="q-electron-drag"></MacControl>
-    </template>
+    <Transition name="mac-control">
+      <MacControl v-if="$q.platform.is.mac && !viewStore.leftDrawerOpen" class="q-electron-drag"></MacControl>
+    </Transition>
   </q-toolbar>
 </template>
 
 <style scoped>
+.mac-control-enter-active,
+.mac-control-leave-active {
+  transition: all 0.3s ease;
+  overflow: hidden;
+}
 
+.mac-control-enter-from {
+  width: 0;
+  opacity: 0;
+}
+
+.mac-control-enter-to {
+  width: 50px;
+  opacity: 1;
+}
+
+.mac-control-leave-from {
+  width: 50px;
+  opacity: 1;
+}
+
+.mac-control-leave-to {
+  width: 0;
+  opacity: 0;
+}
 </style>
